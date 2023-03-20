dc7546b458185f028341ff2c5e69d5c2944bb830
nano235 <revivaladolor@yahoo.com>
2023-03-20 21:47:00 +0100

    second commit

--- components/cards/LongCard.tsx
+++ components/cards/LongCard.tsx
@@ -1,38 +1,42 @@
-function LongCard({ ITLongCard }) {
-  return (
-    <div className="container">
-      <div className="row">
-        <div className="col-12">
-          <div className="feature-list__three">
-            <div className="row">
-              {ITLongCard.map((card, index) => {
-                const { icon, title, content } = card;
-                return (
-                  <div className="col-lg-6 wow move-up" key={index}>
-                    <div className="grid-item animate">
-                      <div className="ht-box-icon style-03">
-                        <div className="icon-box-wrap">
-                          <div className="content-header">
-                            <div className="icon">
-                              <i className={icon}></i>
-                            </div>
-                            <h5 className="heading">{title}</h5>
-                          </div>
-                          <div className="content">
-                            <div className="text">{content}</div>
-                          </div>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                );
-              })}
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
+function LongCard({ ITLongCard }: any) {
+	return (
+		<div className="container">
+			<div className="row">
+				<div className="col-12">
+					<div className="feature-list__three">
+						<div className="row">
+							{ITLongCard.map((card: any, index: number) => {
+								const { icon, title, content } = card;
+								return (
+									<div className="col-lg-6 wow move-up" key={index}>
+										<div className="grid-item animate">
+											<div className="ht-box-icon style-03">
+												<div className="icon-box-wrap">
+													<div className="content-header">
+														<div className="icon">
+															<i className={icon}></i>
+														</div>
+														<h5 className="heading">
+															{title}
+														</h5>
+													</div>
+													<div className="content">
+														<div className="text">
+															{content}
+														</div>
+													</div>
+												</div>
+											</div>
+										</div>
+									</div>
+								);
+							})}
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 }
 
 export default LongCard;
